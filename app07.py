da0bbf61fe2b84849eed157d2266a510678de97e
N-Pushpalatha <144149770+N-Pushpalatha@users.noreply.github.com>
2023-09-07 19:10:35 +0530

    Add files via upload

---
+++ app07.py
@@ -0,0 +1,8 @@
+a=3
+b=5
+add=a+b
+sub=a-b
+multiple=a*b
+print("The addition of two numbers is:",add)
+print("The subtraction of two numbers is:",sub)
+print("The multiply of two numbers is:",multiple)
